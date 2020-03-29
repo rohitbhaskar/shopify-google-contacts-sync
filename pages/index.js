9ff30eff2441291aaa8552e03cda6c25f9b29606
Rohit Bhaskar <rohit@elevarsports.com>
2020-03-29 19:32:16 +0530

    working on the UI part of the tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { TextStyle } from '@shopify/polaris';
+
 const Index = () => (
   <div>
-    <p>Sample app using React and Next.js</p>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
   </div>
 );
 
